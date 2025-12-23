85bacf31098a30de8df0ae6e1788b00a24c4dd45
@champ.1136938 <champ.1136938@gmail.com>
2025-12-23 21:10:31 +0700

    test script typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const mynumber:number[] = [1,2,3,4]
   return (
     <>
       <div>
-        HELLO
+        {mynumber.map((item , index)=>(
+          <>
+          <li key={index}>{item}</li>
+          </>
+        ))}
       </div>
     </>
   )
